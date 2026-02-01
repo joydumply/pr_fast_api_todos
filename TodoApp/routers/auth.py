f6544fcd02431947f0594e7e2c7e5ad471a50cf6
Nikita Babichev <nick.babichev@gmail.com>
2026-02-02 01:48:06 +0200

    created authentication

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -7,6 +7,7 @@ from sqlalchemy.orm import Session
 from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 #app = FastAPI() # Creates a new instance of APP
 
@@ -31,6 +32,14 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     #create_user_model = Users(**create_user_request.model_dump()) # Will fire an error because CreateUserRequest has "password" field, not "hashed_password" field
@@ -48,3 +57,9 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     db.add(create_user_model)
     db.commit()
 
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return 'Failed Auth'
+    return 'Successful Auth'
