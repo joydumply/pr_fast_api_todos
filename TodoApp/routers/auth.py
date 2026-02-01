51b29e64653da1a93fa83d39e121f42b8266a4fa
Nikita Babichev <nick.babichev@gmail.com>
2026-02-02 01:22:23 +0200

    Storing created users to DB

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,10 +1,18 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from database import SessionLocal
 from models import Users
+from passlib.context import CryptContext
 
 #app = FastAPI() # Creates a new instance of APP
 
 router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -14,9 +22,17 @@ class CreateUserRequest(BaseModel):
     password: str
     role: str
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-@router.post("/auth")
-async def create_user(create_user_request: CreateUserRequest):
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     #create_user_model = Users(**create_user_request.model_dump()) # Will fire an error because CreateUserRequest has "password" field, not "hashed_password" field
 
     create_user_model = Users(
@@ -25,8 +41,10 @@ async def create_user(create_user_request: CreateUserRequest):
         first_name=create_user_request.first_name,
         last_name=create_user_request.last_name,
         role=create_user_request.role,
-        hashed_password=create_user_request.password,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
 
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
+
